032cdd77b42ea9672940391ef75e2acf2c856b6e
Jose Rodriguez <j_rodriguez_arruela@hotmail.com>
2026-07-30 12:49:05 -0500

    Call Components based in Type Of Service #13

--- frontend/crm-sicotyc-web/src/app/pages/dashboard/dashboard.component.ts
+++ frontend/crm-sicotyc-web/src/app/pages/dashboard/dashboard.component.ts
@@ -1,15 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ImportacionComponent } from '../../components/sicotyc/importacion/importacion.component';
-import { ExportacionComponent } from "../../components/sicotyc/exportacion/exportacion.component";
-import { CargaSueltaComponent } from "../../components/sicotyc/carga-suelta/carga-suelta.component";
+import { ExportacionComponent } from '../../components/sicotyc/exportacion/exportacion.component';
+import { CargaSueltaComponent } from '../../components/sicotyc/carga-suelta/carga-suelta.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [ImportacionComponent, ExportacionComponent, CargaSueltaComponent],
+  imports: [CommonModule, ImportacionComponent, ExportacionComponent, CargaSueltaComponent],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+  selectedService: 'default' | 'importacion' | 'exportacion' | 'carga-suelta' = 'importacion';
 
+  onServiceChange(event: Event): void {
+    const target = event.target as HTMLSelectElement;
+    this.selectedService = (target.value as 'default' | 'importacion' | 'exportacion' | 'carga-suelta');
+  }
 }
