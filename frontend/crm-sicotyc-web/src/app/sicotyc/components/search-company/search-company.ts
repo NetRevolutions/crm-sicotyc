ee5ea56cb8604783b27b94a5d2a325c90b4da7ab
Jose Rodriguez <j_rodriguez_arruela@hotmail.com>
2026-08-10 11:14:24 -0500

    Update Architecture CR<-Sicotyc (UI) #18

--- frontend/crm-sicotyc-web/src/app/sicotyc/components/search-company/search-company.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-search-company',
-  imports: [],
-  templateUrl: './search-company.html',
-  styles: ``,
-})
-export class SearchCompany {}
